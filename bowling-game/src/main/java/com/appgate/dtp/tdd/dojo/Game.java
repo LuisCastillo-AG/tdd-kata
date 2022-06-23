ddd47a99b7aa97506dc326103530391094590648
luis.castillo <luis.castillo@appgate.com>
2022-06-23 17:24:12 -0500

    test: Add TDD method for write unit test

--- bowling-game/src/main/java/com/appgate/dtp/tdd/dojo/Game.java
+++ bowling-game/src/main/java/com/appgate/dtp/tdd/dojo/Game.java
@@ -2,5 +2,40 @@ package com.appgate.dtp.tdd.dojo;
 
 public class Game {
 
+    private int score;
+    private int tries;
+    private int round;
+
+    public void roll(int i) {
+
+        if (tries == 2)
+            changeRound();
+
+
+        if (i > 10 || i < 0) {
+            throw new IllegalStateException("Error invalid pins take down");
+        }
+        tries += 1;
+        if(tries>2){
+            throw new IllegalStateException("Error invalid tries");
+        }
+        if (tries == 2) {
+            round += 1;
+        }
+        score += i;
+    }
+
+
+    public int score() {
+        return score;
+    }
+
+    public int round() {
+        return round;
+    }
+    public void changeRound(){
+        round +=1;
+        tries=0;
+    }
 }
 
--- bowling-game/src/test/java/com/appgate/dtp/tdd/dojo/GameTest.java
+++ bowling-game/src/test/java/com/appgate/dtp/tdd/dojo/GameTest.java
@@ -1,5 +1,7 @@
 package com.appgate.dtp.tdd.dojo;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +10,86 @@ class GameTest {
 
     @Test
     @DisplayName("readable test name")
-    void shouldDoSomething() {
-
+    void shouldReturnScoreWhenRoll() {
+        //Given
+        var game = new Game();
+        //When
+        game.roll(2);
+        //Then
+        Assert.assertEquals(game.score(), 2);
     }
+
+    @Test
+    @DisplayName("readable test name")
+    void shouldReturnScoreWhenRollItsMoreThanOne() {
+        //Given
+        var game = new Game();
+        //When
+        game.roll(2);
+        game.roll(2);
+        //Then
+        Assert.assertEquals(game.score(), 4);
+    }
+
+
+    @Test
+    @DisplayName("shouldReturnErrorWhenMoreThanLimit")
+    void shouldReturnErrorWhenMoreThanLimit() {
+        //Given
+        var game = new Game();
+        //When
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            //Code under test
+            game.roll(12);
+        });
+        Assertions.assertEquals("Error invalid pins take down", thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("shouldReturnErrorWhenMoreThanLimit")
+    void shouldReturnErrorWhenMoreLessThanZero() {
+        //Given
+        var game = new Game();
+        //When
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            //Code under test
+            game.roll(-3);
+        });
+        Assertions.assertEquals("Error invalid pins take down", thrown.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("shouldCanPlayTenTries")
+    void shouldCanPlayTenTries() {
+        //Given
+        var game = new Game();
+        //When
+        game.roll(2);
+        game.roll(2);
+
+        game.roll(2);
+        game.roll(2);
+
+        game.roll(2);
+        game.roll(2);
+
+        // Then
+        Assertions.assertEquals(3,  game.round());
+    }
+//
+//    @Test
+//    @DisplayName("shouldJumpARoundWhenDropAllPinesInOneTry")
+//    void shouldJumpARoundWhenDropAllPinesInOneTry() {
+//        //Given
+//        var game = new Game();
+//        //When
+//        game.roll(10);
+//        game.roll(2);
+//
+//        // Then
+//        Assertions.assertEquals(2,  game.round());
+//    }
+
+
 }
